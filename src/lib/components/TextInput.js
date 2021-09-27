56db43970a0c46cc17aca2190059b57fddf35509
NiverMtz <nivermartinezmed@gmail.com>
2021-09-27 00:48:54 -0500

    readme

---
+++ src/lib/components/TextInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./TextInput.css";
+
+const TextInput = ({
+  type = "text",
+  label,
+  placeholder,
+  value,
+  onChange,
+  helpText,
+}) => {
+  return (
+    <div className="simple-form-group">
+      {label && <label className="simple-text-label">{label}</label>}
+      <input
+        type={type}
+        className="simple-text-input"
+        value={value}
+        onChange={(e) => onChange && onChange(e.target.value)}
+        placeholder={placeholder}
+      />
+      {helpText && <small className="simple-form-text">{helpText}</small>}
+    </div>
+  );
+};
+
+export default TextInput;
